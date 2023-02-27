6db3b0648d8785948745c08d2e97da5945c9b0bc
Lucas Ferreira <lucas.maia@venhapranuvem.com.br>
2023-02-26 22:46:15 -0300

    Adicionando navegação

--- src/App.js
+++ src/App.js
@@ -1,33 +1,23 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
 
-import Directory from "./components/directory/directory.component";
+import Home from "./router/home/home.component";
+import Navigation from "./router/navigation/navigation.component";
+
+const Shop = () => {
+  return(
+    <h1>Fazendo pedido</h1>
+  );
+};
 
 const App = () => {
-  const categorias = [
-    {
-      title: "Fast Food",
-      imageUrl: "https://images.unsplash.com/photo-1550547660-d9450f859349?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=765&q=80"
-    },
-    {
-      title: "Doces",
-      imageUrl: "https://images.unsplash.com/photo-1624683027339-1d9d5414f628?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1408&q=80"
-    },
-    {
-      title: "Massas",
-      imageUrl: "https://images.unsplash.com/photo-1612892010343-983bfd063dc5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80"
-    },
-    {
-      title: "Frutos-do-mar",
-      imageUrl: "https://images.unsplash.com/photo-1571167366136-b57e07761625?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80"
-    },
-    {
-      title: "Sobremesas",
-      imageUrl: "https://images.unsplash.com/photo-1551024506-0bccd828d307?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80"
-    },
-  ];
-
   return (
-    <Directory list={categorias} />
+    <Routes>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path="shop" element={<Shop />} />
+      </Route>
+    </Routes>
   );
 };
 
---
+++ src/router/navigation/navigation.component.jsx
@@ -0,0 +1,24 @@
+import React, { Fragment } from "react";
+import { Link, Outlet } from "react-router-dom";
+
+import './navigation.style.scss'
+
+const Navigation = () => {
+  return (
+    <Fragment>
+      <div className="navigation">
+        <Link className="logo-container" to="/">
+          <div className="logo">LOGO</div>
+        </Link>
+        <div className="nav-links-container">
+          <Link className="nav-link" to="shop">
+            Teste
+          </Link>
+        </div>
+      </div>
+      <Outlet />
+    </Fragment>
+  );
+};
+
+export default Navigation;
